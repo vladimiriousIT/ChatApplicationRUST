833c0ecaad4f3737c9027050eb7fe6bc6238d885
Vladimir Stratiev <vladimir_stratiev@yahoo.com>
2018-06-15 11:24:35 +0300

    Add files via upload

---
+++ client/target/debug/client.d
@@ -0,0 +1,1 @@
+/Users/stratvla/Documents/Projects/Rust/chatAppRUST/client/target/debug/client.dSYM: /Users/stratvla/Documents/Projects/Rust/chatAppRUST/client/src/main.rs
---
+++ client/target/debug/deps/client-1b67a7b66c54ea03.d
@@ -0,0 +1,5 @@
+/Users/stratvla/Documents/Projects/Rust/chatAppRUST/client/target/debug/deps/client-1b67a7b66c54ea03: src/main.rs
+
+/Users/stratvla/Documents/Projects/Rust/chatAppRUST/client/target/debug/deps/client-1b67a7b66c54ea03.d: src/main.rs
+
+src/main.rs:
